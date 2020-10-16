729cb55cb2c88845f85508052d9a5994070d37a2
BUDAPEST\Zoltan_Limpek <v-zlimpek@expedia.com>
2020-10-16 14:47:33 +0200

    Add udemy test: - add new stepdef class - enable step

---
+++ src/test/java/stepdefinition/MainPageStep.java
@@ -0,0 +1,21 @@
+package stepdefinition;
+
+import cucumber.api.java.en.And;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static java.time.Duration.ofSeconds;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
+public class MainPageStep extends BaseStepDefinition {
+
+    @And("I close the cookie policy overlay")
+    public void i_close_the_cookie_policy_overlay() {
+        WebDriverWait waiter = new WebDriverWait(driver, 10);
+        waiter.withTimeout(ofSeconds(5)).until(presenceOfElementLocated(By.className("legal-notice--close-btn--2BZfw")));
+
+        WebElement closeCookiePolicyOverlayButton = driver.findElement(By.className("legal-notice--close-btn--2BZfw"));
+        closeCookiePolicyOverlayButton.click();
+    }
+}
