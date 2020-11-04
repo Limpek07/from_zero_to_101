42d8403fc9576b4dddbd81d24c7a00cd92b819bf
BUDAPEST\Zoltan_Limpek <v-zlimpek@expedia.com>
2020-11-04 15:22:50 +0100

    Add udemy test: - add assertion methods to page object - add new elements to page object - add new step - enable steps - add step definitions

--- src/main/java/com/zeroone/pageobject/SearchResultsPage.java
+++ src/main/java/com/zeroone/pageobject/SearchResultsPage.java
@@ -1,11 +1,15 @@
 package com.zeroone.pageobject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 import static com.zeroone.util.WebElementHandler.waitForElementToBeVisible;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Page object for the Search Results page.
@@ -15,11 +19,29 @@ import static com.zeroone.util.WebElementHandler.waitForElementToBeVisible;
 public class SearchResultsPage {
     private ChromeDriver driver;
 
+    private By link = By.tagName("a");
+    private By title = By.cssSelector("[class*='course-card--course-title--']");
+
     @FindBy(css = "[class*='search--header-container']")
     private WebElement searchHeaderContainer;
 
+    @FindBy(css = "[class*='course-list--container--'] [class*='popper--popper--']")
+    private List<WebElement> courses;
+
     public void waitForSearchHeaderContainerToBeVisible() {
         waitForElementToBeVisible("search header container", driver, searchHeaderContainer);
+    }
+
+    public void validateFirstCourseLinkUrl(String expectedUrl) {
+        assertThat(courses.get(0).findElement(link).getAttribute("href")).isEqualTo(expectedUrl);
+    }
+
+    public void validateNumberOfCourses(int expectedNumberOfCourses) {
+        assertThat(courses.size()).isEqualTo(expectedNumberOfCourses);
+    }
+
+    public void validateSecondCourseTitle(String expectedCourseTitle) {
+        assertThat(courses.get(1).findElement(title).getText()).isEqualTo(expectedCourseTitle);
     }
 
     public SearchResultsPage(ChromeDriver driver) {
--- src/test/java/stepdefinition/SearchResultsStep.java
+++ src/test/java/stepdefinition/SearchResultsStep.java
@@ -1,6 +1,7 @@
 package stepdefinition;
 
 import com.zeroone.pageobject.SearchResultsPage;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 
 /**
@@ -13,5 +14,20 @@ public class SearchResultsStep extends BaseStepDefinition {
     @Then("I should be redirected to the search results page")
     public void i_should_be_redirected_to_the_search_results_page() {
         new SearchResultsPage(driver).waitForSearchHeaderContainerToBeVisible();
+    }
+
+    @And("I should have {int} courses on the page")
+    public void i_should_have_x_courses_on_the_page(int numberOfCourses) {
+        new SearchResultsPage(driver).validateNumberOfCourses(numberOfCourses);
+    }
+
+    @And("the first course should point to {string} url")
+    public void the_first_course_should_point_to_x_url(String url) {
+        new SearchResultsPage(driver).validateFirstCourseLinkUrl(url);
+    }
+
+    @And("the title of the second course should be {string}")
+    public void the_title_of_the_second_course_should_be_x(String courseTitle) {
+        new SearchResultsPage(driver).validateSecondCourseTitle(courseTitle);
     }
 }
