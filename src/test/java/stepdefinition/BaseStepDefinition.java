d74cae0b859202967e0394c7203884ed5a8713b7
BUDAPEST\Zoltan_Limpek <v-zlimpek@expedia.com>
2020-11-09 15:31:55 +0100

    Add udemy test: - add javadoc

--- src/test/java/stepdefinition/BaseStepDefinition.java
+++ src/test/java/stepdefinition/BaseStepDefinition.java
@@ -4,6 +4,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import static com.zeroone.driver.Driver.getChromeDriver;
 
+/**
+ * For data, fields, methods that reoccur regularly in step definition classes.
+ *
+ * @author Zoltan_Limpek
+ */
 public class BaseStepDefinition {
     ChromeDriver driver = getChromeDriver();
 }
